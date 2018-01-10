d8eef153ad118232ffff57fa2ac94260c2de96f1
Jeremy Townson <jeremy.townson@gmail.com>
2018-01-10 17:49:40 +0000

    Add solution to meetup.

--- exercises/meetup/src/main/scala/Meetup.scala
+++ exercises/meetup/src/main/scala/Meetup.scala
@@ -1,10 +1,43 @@
+
+import java.time.temporal.TemporalAdjusters.firstInMonth
 import java.time.{DayOfWeek, LocalDate}
 
 import Schedule.Schedule
 
 case class Meetup(month: Int, year: Int) {
 
-  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = ???
+  def day(dayOfWeek: Int, schedule: Schedule): LocalDate = {
+
+    val dim = daysInMonth(dayOfWeek)
+
+    schedule match {
+      case Schedule.First => dim(0)
+      case Schedule.Second => dim(1)
+      case Schedule.Third => dim(2)
+      case Schedule.Fourth => dim(3)
+      case Schedule.Last => dim.last
+      case Schedule.Teenth => dim.filter(isTeenth).head
+    }
+  }
+
+  private def daysInMonth(dayOfWeek: Int): Seq[LocalDate] = {
+    new Iterator[LocalDate] {
+
+      var current: LocalDate = LocalDate.of(year, month, 1).`with`(firstInMonth(DayOfWeek.of(dayOfWeek))).minusDays(7)
+
+      override def hasNext: Boolean = current.plusDays(7).getMonthValue == month
+
+      override def next(): LocalDate = {
+        current = current.plusDays(7)
+        current
+      }
+    }.toSeq
+  }
+
+  def isTeenth(localDate: LocalDate): Boolean = {
+    val dayOfMonth = localDate.getDayOfMonth
+    12 < dayOfMonth && dayOfMonth < 20
+  }
 }
 
 object Schedule extends Enumeration {
