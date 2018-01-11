452aae1b5cfab79c70c70a0160bb56d5ecb10ed2
Jeremy Townson <jeremy.townson@gmail.com>
2018-01-11 15:00:34 +0000

    Add solution to raindrops.

--- exercises/raindrops/src/main/scala/Raindrops.scala
+++ exercises/raindrops/src/main/scala/Raindrops.scala
@@ -1,4 +1,10 @@
 object Raindrops {
-  def convert(n: Int): String = ???
-}
 
+  private val factors = Seq(3 -> "Pling", 5 -> "Plang", 7 -> "Plong")
+
+  def convert(n: Int): String =
+    factors.collect({ case (i, word) if n % i == 0 => word }).mkString match {
+      case "" => n.toString
+      case s  => s
+    }
+}
